85efdc19137caa01a21862d67b0265ce7f4b6d5c
yuanyehome <2252706531@qq.com>
2020-05-07 01:34:55 +0800

    [yy] add transformer

--- src/transformer/Models.py
+++ src/transformer/Models.py
@@ -6,4 +6,159 @@ import torch
 import torch.nn as nn
 import numpy as np
 from transformer.layers import EncoderLayer, DecoderLayer
+from utils import get_pad_mask, get_subsequent_mask
 
+
+def _get_sin_encoding_table(n_position, d_hid):
+    """
+    :param n_position:
+    :param d_hid:
+    :return:
+    """
+
+    def get_position_angle(position):
+        return [position / np.power(10000, 2 * (idx // 2) / d_hid)
+                for idx in range(d_hid)]
+
+    sin_table = np.array([get_position_angle(pos_idx)
+                          for pos_idx in range(n_position)])
+    sin_table[:, 0::2] = np.sin(sin_table[:, 0::2])
+    sin_table[:, 1::2] = np.cos(sin_table[:, 1::2])
+
+    return torch.tensor(sin_table, dtype=torch.float).unsqueeze(0)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_hid, n_position=200):
+        super(PositionalEncoding, self).__init__()
+        # 不将这个table视为参数，而是常量
+        self.register_buffer('pos_table', _get_sin_encoding_table(n_position, d_hid))
+
+    def forward(self, x):
+        return x + self.pos_table[:, :x.size(1)].clone().detach()
+
+
+class Encoder(nn.Module):
+    def __init__(self, vocab_size, embed_dim, n_layers, n_head, d_k, d_v, d_model,
+                 d_inner, pad_idx, dropout=0.1, n_position=200):
+        """
+        :param vocab_size: 字典大小
+        :param embed_dim: 词嵌入维度
+        :param n_layers: 有多少个encoderlayer
+        :param n_head: multihead数量
+        :param d_k: key和query的维度（参考论文）
+        :param d_v: value的维度（参考论文）
+        :param d_model: 和embed_dim应该一直保持一致
+        :param d_inner: 全连接层隐层维度
+        :param pad_idx: TODO 和nn.Embedding层有关，需要继续调查
+        :param n_position: TODO
+        """
+        super(Encoder, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_dim, padding_idx=pad_idx)
+        self.position_encoding = PositionalEncoding(embed_dim, n_position)
+        self.dropout = nn.Dropout(dropout)
+        self.layers = nn.ModuleList([
+            EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
+            for _ in range(n_layers)
+        ])
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+    def forward(self, in_seq, mask, return_attn=False):
+        """
+        :param in_seq:
+        :param mask:
+        :param return_attn:
+        """
+        enc_self_attn_list = []
+        enc_output = self.dropout(self.position_encoding(self.embed(in_seq)))
+
+        for enc_layer in self.layers:
+            enc_output, enc_self_attn = enc_layer(enc_output, mask=mask)
+            enc_self_attn_list.append(enc_self_attn)
+        enc_output = self.layer_norm(enc_output)
+
+        if return_attn:
+            return enc_output, enc_self_attn_list
+        else:
+            return enc_output
+
+
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, embed_dim, n_layers, n_head, d_k, d_v, d_model,
+                 d_inner, pad_idx, dropout=0.1, n_position=200):
+        super(Decoder, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_dim, padding_idx=pad_idx)
+        self.position_encoding = PositionalEncoding(embed_dim, n_position)
+        self.dropout = nn.Dropout(dropout)
+        self.layers = nn.ModuleList([
+            DecoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
+            for _ in range(n_layers)
+        ])
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+    def forward(self, seq, dec_mask, encoder_output, enc_mask, return_attns=False):
+        dec_self_attn_list, dec_enc_attn_list = [], []
+
+        dec_output = self.dropout(self.position_encoding(self.embed(seq)))
+        for dec_layer in self.layers:
+            dec_output, dec_self_attn, dec_enc_attn = dec_layer(
+                dec_output, encoder_output,
+                slf_attn_mask=dec_mask,
+                dec_enc_attn_mask=enc_mask
+            )
+            dec_self_attn_list.append(dec_self_attn)
+            dec_enc_attn_list.append(dec_enc_attn)
+        dec_output = self.layer_norm(dec_output)
+        if return_attns:
+            return dec_output, dec_self_attn_list, dec_enc_attn_list
+        else:
+            return dec_output
+
+
+class Transformer(nn.Module):
+    def __init__(self, vocab_size, in_pad_idx, out_pad_idx,
+                 embed_dim=300, d_model=300, d_inner=2048, n_layers=6,
+                 n_head=8, d_k=64, d_v=64, dropout=0.1, n_position=200,
+                 ):
+        """
+        :param vocab_size:
+        :param in_pad_idx:
+        :param out_pad_idx:
+        :param embed_dim:
+        :param d_model:
+        :param d_inner:
+        :param n_layers:
+        :param n_head:
+        :param d_k:
+        :param d_v:
+        :param dropout:
+        :param n_position:
+        """
+        # TODO 预训练词向量
+        super(Transformer, self).__init__()
+        assert (d_model == embed_dim)
+        self.in_pad_idx = in_pad_idx
+        self.out_pad_idx = out_pad_idx
+        self.encoder = Encoder(
+            vocab_size, embed_dim, n_layers, n_head,
+            d_k, d_v, d_model, d_inner, in_pad_idx, dropout, n_position
+        )
+        self.decoder = Decoder(
+            vocab_size, embed_dim, n_layers, n_head,
+            d_k, d_v, d_model, d_inner, in_pad_idx, dropout, n_position
+        )
+        self.output_layer = nn.Linear(d_model, vocab_size, bias=False)
+
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def forward(self, in_seq, target_seq):
+        in_mask = get_pad_mask(in_seq, self.in_pad_idx)
+        target_mask = \
+            get_pad_mask(target_seq, self.out_pad_idx) & get_subsequent_mask(target_seq)
+        enc_output, *_ = self.encoder(in_seq, in_mask)
+        dec_output, *_ = self.decoder(target_seq, target_mask, enc_output, in_mask)
+        dec_output = self.output_layer(dec_output)
+
+        return dec_output.view(-1, dec_output.size(2))
--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,30 @@
 """
 
 import numpy as np
+import torch
 
 
 def get_pad_mask(seq, pad_idx):
     """
     :param seq:
-    :param pad_idx: 
+    :param pad_idx:
     :return:
     """
     return (seq != pad_idx).unsqueeze(-2)
+
+
+def get_subsequent_mask(seq):
+    """
+    :param seq:
+    :return:
+    """
+    sz_b, len_s = seq.size()
+    subsequent_mask = (-torch.triu(
+        torch.ones((1, len_s, len_s), device=seq.device), diagonal=1
+    ) + 1).bool()
+    return subsequent_mask
+
+
+if __name__ == "__main__":
+    t = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    print("...")
