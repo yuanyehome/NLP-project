02a925b7cae26d8cd80ef2c58dfdd3701c9df976
yuanyehome <2252706531@qq.com>
2020-05-08 14:20:12 +0800

    complete translator

--- src/transformer/translator.py
+++ src/transformer/translator.py
@@ -63,5 +63,50 @@ class Translator(nn.Module):
         encoder_output = encoder_output.repeat(beam_size, 1, 1)
         return encoder_output, gen_seq, scores
 
+    def _get_the_best_score_and_idx(self, gen_seq, decoder_output, scores, step):
+        """
+        :param gen_seq:
+        :param decoder_output:
+        :param scores:
+        :param step:
+        :return:
+        """
+        beam_size = self.beam_size
+        best_k2_probs, best_k2_idx = decoder_output[:, -1, :].topk(beam_size)
+        scores = torch.log(best_k2_probs).view(beam_size, -1) + scores.view(beam_size, 1)
+        scores, best_k_idx_in_k2 = scores.view(-1).topk(beam_size)
 
+        _r, _k = best_k_idx_in_k2 // beam_size, best_k_idx_in_k2 % beam_size
+        best_k_idx = best_k2_idx[_r, _k]
+        gen_seq[:, :step] = gen_seq[_r, :step]
+        gen_seq[:, step] = best_k_idx
 
+        return gen_seq, scores
+
+    def translate_sentence(self, input_seq):
+        assert input_seq.size(0) == 1
+        pad_idx = self.pad_idx
+        eos_idx = self.eos_idx
+        max_seq_len = self.max_seq_len
+        beam_size = self.beam_size
+        alpha = self.alpha
+
+        with torch.no_grad():
+            input_mask = get_pad_mask(input_seq, pad_idx)
+            encoder_output, gen_seq, scores = self._get_init_state(input_seq, input_mask)
+
+            ans_idx = 0
+            for step in range(2, max_seq_len):
+                decoder_output = self._model_decode(gen_seq[:, :step],
+                                                    encoder_output, input_mask)
+                gen_seq, scores = self._get_the_best_score_and_idx(
+                    gen_seq, decoder_output, scores, step
+                )
+                eos_locs = gen_seq == eos_idx
+                seq_lens, _ = self.len_map.masked_fill(~eos_locs, max_seq_len).min(1)
+                # TODO: 上面那句话在干啥
+                if (eos_locs.sum(1) > 0).sum(0).item() == beam_size:
+                    _, ans_idx = scores.div(seq_lens.float() ** alpha).max(0)
+                    ans_idx = ans_idx.item()
+                    break
+        return gen_seq[ans_idx][:seq_lens[ans_idx]].tolist()
